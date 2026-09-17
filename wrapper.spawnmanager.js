973a18ad192bf0eac023998e87456c15395dbf42
SimonBailey5666 <SimonBailey5666@gmail.com>
2026-09-17 19:07:03 +1000

    fix?

--- wrapper.spawnmanager.js
+++ wrapper.spawnmanager.js
@@ -12,12 +12,12 @@ class SpawnManager {
     run(tickOffset) {
 
         if(common.atTick(15, tickOffset)){
-            updateSpawnQueue();
+            this.updateSpawnQueue();
             
             //Queue is getting backlogged, sort it by creep priority so economy doesnt crash
             if(Memory.rooms[this.roomName].spawnQueue.length > 10 && Game.time - Memory.rooms[this.roomName].sortQueue > 100){
                 Memory.rooms[this.roomName].sortQueue = Game.time;
-                sortQueue();
+                this.sortQueue();
             }
         } 
 
