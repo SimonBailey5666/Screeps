2e56fc3f8a1c57e1428c71caec8c20de41ba0b9b
SimonBailey5666 <SimonBailey5666@gmail.com>
2026-09-18 08:57:10 +1000

    repair logic

--- class.tower.js
+++ class.tower.js
@@ -33,16 +33,24 @@ class TowerManager {
             });
             if (damagedCreeps) {
                 tower.heal(damagedCreeps);
+                return;
             }
             
             const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: structure => (structure.hits < structure.hitsMax && structure.structureType !== STRUCTURE_WALL)
+                filter: structure => (structure.hits < structure.hitsMax && structure.structureType !== STRUCTURE_WALL && structure.structureType !== STRUCTURE_RAMPART)
             });
         
             if (closestDamagedStructure) {
                 tower.repair(closestDamagedStructure);
                 return;
             }
+
+            const closestDamagedFort = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: structure => (structure.hits < 100000 && structure.structureType !== STRUCTURE_WALL && structure.structureType !== STRUCTURE_RAMPART)
+            });
+
+            tower.repair(closestDamagedFort);
+            return;
         
             
         }
