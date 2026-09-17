1004f638707e3e5979a44df772a97288c5f3c0ae
SimonBailey5666 <SimonBailey5666@gmail.com>
2026-09-18 08:10:15 +1000

    fix

--- role.healer.js
+++ role.healer.js
@@ -66,7 +66,7 @@ var roleHealer = {
                 creep =>
                     creep.memory.squadLeader &&
                     creep.memory.work === sCreep.memory.work &&
-                    creep.pos.roomName === sCreep.pos.roomName &&
+                    creep.memory.home === sCreep.pos.roomName &&
                     creep.memory.subordinates.length < SQUAD_SIZE - 1
             );
 
