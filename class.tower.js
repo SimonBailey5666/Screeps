b8168673fa4729c8f97dbdd274114267d3164c98
SimonBailey5666 <SimonBailey5666@gmail.com>
2026-09-18 10:33:18 +1000

    test

--- class.tower.js
+++ class.tower.js
@@ -52,7 +52,8 @@ class TowerManager {
             const room = Game.rooms[this.roomName];
             if(room.energyCapacityAvailable - room.energyAvailable < 300){
                 const closestDamagedFort = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: structure => (structure.hits / structure.hitsMax < 0.1 && (structure.structureType === STRUCTURE_WALL || structure.structureType === STRUCTURE_RAMPART))});
+                filter: structure => (structure.hits / structure.hitsMax < 0.1 && (structure.structureType === STRUCTURE_WALL || structure.structureType === STRUCTURE_RAMPART))
+                }).sort((a, b) => a.hits - b.hits)[0];
             
                 tower.repair(closestDamagedFort);
             }         
