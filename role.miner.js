9578e96f74cbb2114ed56967b14b9c35933c16e3
SimonBailey5666 <SimonBailey5666@gmail.com>
2026-09-19 23:07:54 +1000

    logging

--- role.miner.js
+++ role.miner.js
@@ -35,7 +35,7 @@ var roleMiner = {
                 || structure.structureType === STRUCTURE_LINK)
             });
             if(container){
-                console.log(creep.name + " here");
+                console.log(creep.name + " here " + container.id);
                 creep.memory.storage = container.id;
             }
             else {
