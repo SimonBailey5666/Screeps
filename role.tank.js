ffc92f1cce74e974339179de6249f9a642b5f61d
SimonBailey5666 <SimonBailey5666@gmail.com>
2026-09-19 18:20:17 +1000

    fix

--- role.tank.js
+++ role.tank.js
@@ -37,8 +37,9 @@ var roleTank = {
             }
             //If not at max health go to rallypoint
             if(creep.memory.rallypoint && Game.flags[creep.memory.rallypoint] && !creep.memory.procced){
-                if(creep.hits === creep.hitsMax && !creep.memory.rallyFirst){
+                if(creep.hits === creep.hitsMax && creep.memory.rallyFirst){
                     creep.memory.procced = true;
+                    return;
                 }
 
                 creep.moveTo(Game.flags[creep.memory.rallypoint])
