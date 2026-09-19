a77bf610b905cac6d73ed26816b7fa8805484402
SimonBailey5666 <SimonBailey5666@gmail.com>
2026-09-19 23:05:25 +1000

    logging

--- role.miner.js
+++ role.miner.js
@@ -30,6 +30,7 @@ var roleMiner = {
         
         source = Game.getObjectById(creep.memory.target);
         if(!creep.memory.storage){
+            console.log("here");
             let container = source.pos.findInRange(FIND_STRUCTURES, 1, {
                 filter: (structure) => (structure.structureType === STRUCTURE_CONTAINER
                 || structure.structureType === STRUCTURE_LINK)
