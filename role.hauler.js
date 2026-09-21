833e0b81a810ac1d5a3621de29d599cf8f22e928
SimonBailey5666 <SimonBailey5666@gmail.com>
2026-09-22 09:48:13 +1000

    wip

--- role.hauler.js
+++ role.hauler.js
@@ -95,6 +95,7 @@ var roleHauler = {
                 filter: structure => (structure.structureType == STRUCTURE_CONTAINER) &&  
                 (structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0)
             });
+            
             if(targets.length>0){
                 console.log("targets: " +targets);
                 creep.memory.storage = common.setTarget(targets);
