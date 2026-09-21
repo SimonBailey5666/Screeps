88f4947f3d595eadaa96f68e450e7670c88ea097
SimonBailey5666 <SimonBailey5666@gmail.com>
2026-09-21 19:06:52 +1000

    wip

--- roompop.js
+++ roompop.js
@@ -9,7 +9,7 @@ const POPS = {
             upgrader: {
                 minCost: 800,
                 maximumCost: 800,
-                max: 2
+                max: 1
             },
             builder: {
                 minCost: 500,
