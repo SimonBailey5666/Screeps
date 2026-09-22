84ef14723a21f1d210cd138484667d63cf7a75ac
SimonBailey5666 <SimonBailey5666@gmail.com>
2026-09-22 10:03:16 +1000

    pop

--- roompop.js
+++ roompop.js
@@ -230,7 +230,21 @@ const POPS = {
                 max: 1,
                 spawnIf: tCreep =>  !spawnRules.roomControlledByEnemy(tCreep)
             }
-        }
+        },
+        W37S3: {    //ATTACK THIS ROOM
+            defender: {
+                minCost: 800,
+                maximumCost: 800,
+                max: 4,
+                spawnIf: spawnRules.enemiesPresent
+            },
+            scout: {
+                minCost: 50,
+                maximumCost: 50,
+                max: 1,
+                spawnIf: !spawnRules.hasRoomVision
+            }
+        },
     },
     W39S3: {  //new room
         W39S3: { 
